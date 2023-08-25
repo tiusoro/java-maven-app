402ad3c82566d648f1ba8825078d0bcc6ff574a2
Anthony Usoro <33380362+tiusoro@users.noreply.github.com>
2023-08-25 13:33:27 +0100

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,30 @@
+def function() {
+    echo 'function to build the application...'
+
+
+def buildJar() {
+    echo 'building the application...' 
+    sh 'mvn clean package'   
+    
+
+}
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable:'PASS', usernameVariable: 'USER')])
+    sh 'docker build -t tiusoro/java-maven-jenkins:jma-1.0 .'
+    sh "echo $PASS | docker login -u $USER --password-stdin"
+    sh 'docker push tiusoro/java-maven-jenkins:jma-1.0'        
+        
+}
+
+def deployApp() {
+    echo 'deploying the application...'
+
+        
+}
+
+}
+
+
+return this
