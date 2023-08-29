0eb17f15667cb23fd4a48f43d0ee033e9c21eb16
Anthony Usoro <33380362+tiusoro@users.noreply.github.com>
2023-08-29 06:46:38 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,28 +1,22 @@
-def function() {
-    echo 'function to build the application...'
-
-
 def buildJar() {
     echo 'building the application...' 
     sh 'mvn clean package'   
-    
 
 }
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable:'PASS', usernameVariable: 'USER')])
-    sh 'docker build -t tiusoro/java-maven-jenkins:jma-1.0 .'
-    sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh 'docker push tiusoro/java-maven-jenkins:jma-1.0'        
-        
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable:'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t tiusoro/java-maven-jenkins:jma-1.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push tiusoro/java-maven-jenkins:jma-1.0'
+
+    }      
 }
+    
 
 def deployApp() {
     echo 'deploying the application...'
-
-        
-}
 
 }
 
