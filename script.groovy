1ba00e342ea8b5b6ee301343be8bb18c46feca7c
Anthony Usoro <33380362+tiusoro@users.noreply.github.com>
2023-09-01 11:38:36 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,9 +7,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable:'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t tiusoro/java-maven-jenkins:jma-1.7 .'
+        sh 'docker build -t tiusoro/java-maven-jenkins:jma-1.8 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push tiusoro/java-maven-jenkins:jma-1.7'
+        sh 'docker push tiusoro/java-maven-jenkins:jma-1.8'
     } 
 }
     
@@ -17,7 +17,7 @@ def buildImage() {
 def deployApp() {
     echo 'deploying the application...'
     sshagent([‘ec2-server-key’]) {
-	sh “ssh -o StrickHostKeyChecking=no ec2-user@35.180.251.121 ${docker run -p 8080:8080 -d tiusoro/java-maven-app:jma1.7}”
+	sh “ssh -o StrictHostKeyChecking=no ec2-user@35.180.251.121 ${docker run -p 8080:8080 -d tiusoro/java-maven-app:jma1.8}”
     }
 }
 
