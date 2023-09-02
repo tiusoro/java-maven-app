f548da424b0910876f407490446a5b277030ea29
Anthony Usoro <33380362+tiusoro@users.noreply.github.com>
2023-09-02 01:10:41 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -18,7 +18,7 @@ def deployApp() {
     echo 'deploying the application...'
     sshagent([‘ec2-server-key’]) {
 	sh '''
-	    ssh -o StrictHostKeyChecking=no ec2-user@54.242.111.137 
+	    ssh -o StrictHostKeyChecking=no ubuntu@34.207.99.93 
 	    "${docker run -p 8080:8080 -d tiusoro/java-maven-jenkins:jma-2.1}"
 	    '''
     }
