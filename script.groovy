795302ce6b9411b700bf92ad32cb4e8831788d5d
Anthony Usoro <33380362+tiusoro@users.noreply.github.com>
2023-09-20 21:48:36 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -16,9 +16,9 @@ def buildImage() {
 
 def deployApp() {
     echo 'deploying the application...'
-    sshagent([‘ec2-server-key’]) {
-	sh 'ssh -o StrictHostKeyChecking=no ubuntu@34.207.99.93 docker run -p 8080:8080 -d tiusoro/java-maven-jenkins:jma-2.2'
-    }
+ //    sshagent([‘ec2-server-key’]) {
+	// sh 'ssh -o StrictHostKeyChecking=no ubuntu@34.207.99.93 docker run -p 8080:8080 -d tiusoro/java-maven-jenkins:jma-2.2'
+ //    }
 }
 
 
